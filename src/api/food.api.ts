40f2cd0d179dc3446fc094021ec0efa5432f7eb5
babahironobu <ghdtjs0417@gmail.com>
2020-05-26 22:57:10 +0900

    Chor: 타이머 없앰

--- src/api/food.api.ts
+++ src/api/food.api.ts
@@ -10,7 +10,7 @@ const foodMockupData: IFood[] = foodData.data;
 // api 구현 후 안의 내용이 바뀔 수 있음
 // getFoods 함수는 필요한 인자가 없고 promise로 감싸진 IFood의 리스트를 반환한다는 것은 변하지 않음
 const getFoods: () => Promise<IFood[]> = async () => {
-  await timer(1000);
+  // await timer(1000);
   return foodMockupData;
 };
 
